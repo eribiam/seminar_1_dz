e8ac606333524e17c95ccb941c70156949fe9370
dmitriy <boss.vede@mail.ru>
2022-08-09 15:51:05 +0700

    second exercise

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-int max = firstNumber;
-if (secondNumber > max) max = secondNumber;
-System.Console.WriteLine($"Максимальное число = {max}");
+// Console.WriteLine("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// int max = firstNumber;
+// if (secondNumber > max) max = secondNumber;
+// Console.WriteLine($"Максимальное число = {max}");
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+// Console.WriteLine("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+// int max = firstNumber;
+// if (secondNumber > max) max = secondNumber; 
+// if (thirdNumber > max) max = thirdNumber;
+// Console.WriteLine($"Максимальное число = {max}");
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
